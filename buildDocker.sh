f5378850b92057c9e2564eb6a2564934426044bc
vishnu chanderraju <vishnu.ch@cuezen.com>
2023-09-22 22:09:51 +0800

    added comment ot buildDocker.sh

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 echo "exporting poetry deps to requirements.txt..."
+#easy to build docker image with python and install with pip. hence converting to requirements.txt :)
 poetry export --without-hashes --format=requirements.txt > requirements.txt
 echo "building docker image..."
 docker build -t  jaihind213/radio-duck:1.0 -t jaihind213/radio-duck:latest .
