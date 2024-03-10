e4e39dd7867094b5e9c476f78b7322e51061e222
vishnu chanderraju <vishnu.ch@cuezen.com>
2024-03-10 13:00:17 +0800

    simplifying docker build

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,39 @@
-# Use an official Python runtime as a parent image
-ARG DOCKER_DUCKX_IMAGE_VERSION
-FROM jaihind213/duckdbx:${DOCKER_DUCKX_IMAGE_VERSION}
+FROM ubuntu:22.04
+LABEL maintainer="jaihind213@gmail.com"
 
-USER 0
-RUN mkdir -p /radio-duck
-WORKDIR /radio-duck
+ARG PYTHON_VERSION=3.10
+ENV LANG C.UTF-8
+ENV LC_ALL C.UTF-8
+
+RUN useradd -m -d /home/radio-duck radio-duck
+WORKDIR /home/radio-duck
+RUN mkdir "pond"
+
+RUN apt update && apt autoclean && apt install -y software-properties-common && apt update && add-apt-repository ppa:apt-fast/stable -y && add-apt-repository ppa:deadsnakes/ppa && apt update && apt install -y apt-fast
+RUN apt-fast -y install linux-libc-dev libssl-dev
+#RUN apt-fast -y install gcc git g++ libblis64-3-pthread
+RUN apt-fast -y install libblis64-3-pthread
+
+RUN apt-fast update && apt-fast install -y python${PYTHON_VERSION}
+RUN update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${PYTHON_VERSION} 1
+
+#for psutil need to do python3.10-dev u need it later when u do '' pip3 install -r requirements-dev.txt''
+RUN apt-fast -y install python${PYTHON_VERSION}-dev
+RUN apt-fast -y install curl && curl -sS https://bootstrap.pypa.io/get-pip.py | python3 && apt-fast -y remove curl
 
 # Copy the current directory contents into the container at /app
-COPY . /radio-duck/
-COPY default.ini /radio-duck/default.ini
+COPY *.py /home/radio-duck
+COPY requirements.txt /home/radio-duck
+COPY default.ini /home/radio-duck/default.ini
 # Install any needed packages specified in requirements.txt
 RUN pip3 install --no-cache-dir -r requirements.txt
 
 RUN --mount=type=secret,id=duck_sekrets
 #RUN rm -f /etc/ssl/certs/ca-bundle.crt && apt update && apt install --reinstall ca-certificates && update-ca-certificates
-RUN echo "base docker image version: $DOCKER_DUCKX_IMAGE_VERSION" >> base_docker_image_version
+
+RUN mkdir "/quack"
+RUN echo "import duckdb; duckdb.query('install httpfs; load httpfs; install azure; load azure;');" >> /quack/test.py && echo "" >> /quack/test.py
+RUN cd /quack && python3 test.py && cd -
 
 #RUN echo http://dl-cdn.alpinelinux.org/alpine/edge/main >> /etc/apk/repositories
 #RUN apk update && apk add --no-cache zlib-dev==1.3-r2  #->for docker CRITICAL CVE-2023-45853
@@ -22,6 +41,6 @@ RUN echo "base docker image version: $DOCKER_DUCKX_IMAGE_VERSION" >> base_docker
 EXPOSE 8000
 
 # Run server.py when the container launches
-CMD ["python3", "/radio-duck/server.py"]
+CMD ["python3", "/home/radio-duck/server.py"]
 
-VOLUME /radio-duck/pond
+VOLUME /home/radio-duck/pond
--- buildDocker.sh
+++ buildDocker.sh
@@ -2,6 +2,9 @@
 
 PROJECT_VERSION=$1
 DUCKDB_VERSION=$2
+PUSH_REPO=$3
+PUSH_LATEST_TAG=$4
+
 if [ "$PROJECT_VERSION" == "" ];then
   echo "PROJECT_VERSION not set as 1st argument. bash buildDocker.sh <version>  <duckdb_version>"
   exit 2
@@ -10,44 +13,44 @@ if [ "$DUCKDB_VERSION" == "" ];then
   echo "DUCKDB_VERSION not set as 2nd argument. bash buildDocker.sh <project_version> <duckdb_version>"
   exit 2
 fi
+if [ "$PYTHON_VERSION" == "" ];then
+  PYTHON_VERSION=3.10
+fi
+if [ "$DOCKERFILE_VERSION" == "" ];then
+  DOCKERFILE_VERSION=0.1
+fi
 #####################
 export IMAGE_NAME=jaihind213/radio-duck
-export DOCKER_DUCKX_IMAGE_VERSION=$DUCKDB_VERSION-0.3
-IMAGE_VERSION=${PROJECT_VERSION}-${DOCKER_DUCKX_IMAGE_VERSION}
+export IMAGE_VERSION="d${DUCKDB_VERSION}-v${PROJECT_VERSION}-$DOCKERFILE_VERSION"
 PLATFORM=linux/x86_64,linux/amd64
 
-PUSH_REPO=remote
-if [ "$PUSH_REPO" == "local" ];then
-  DOCKER_ARGS="--load" #or --output=type=docker both are same
-else
-  #push to remote
+if [ "$PUSH_REPO" == "remote" ];then
   DOCKER_ARGS="--output=type=registry"
+else
+  DOCKER_ARGS="--load" #or --output=type=docker both are same
 fi
 
-PUSH_LATEST_TAG="no"
+TAGS="-t $IMAGE_NAME:$IMAGE_VERSION"
 if [ "$PUSH_LATEST_TAG" == "yes" ];then
   echo "setting latest tag..."
-  LATEST_TAG = "-t $IMAGE_NAME:latest"
+  TAGS = "$TAGS -t $IMAGE_NAME:latest"
 fi
 
 cd "$(dirname "$0")"
 echo "exporting poetry deps to requirements.txt..."
 #easy to build docker image with python and install with pip. hence converting to requirements.txt :)
 poetry export --without-hashes --format=requirements.txt > requirements.txt
-echo "building docker image... with version $IMAGE_NAME:$IMAGE_VERSION"
-echo "base duckdbX version: $DOCKER_DUCKX_IMAGE_VERSION ..."
-echo "PUSH_REPO FLAG: $PUSH_REPO"
-echo "PUSH_LATEST_TAG FLAG: $PUSH_LATEST_TAG"
-sleep 10
-export DOCKER_BUILDKIT=1
-docker buildx build $DOCKER_ARGS --platform $PLATFORM --build-arg DOCKER_DUCKX_IMAGE_VERSION=${DOCKER_DUCKX_IMAGE_VERSION} -t  $IMAGE_NAME:$IMAGE_VERSION ${LATEST_TAG} .
 
-#for Macs-m1
-#docker buildx build --platform linux/x86_64 -t  $IMAGE_NAME:$PROJECT_VERSION -t $IMAGE_NAME:latest .
+echo "building docker image... with version $IMAGE_NAME:$IMAGE_VERSION"
+echo "PUSH_REPO FLAG: $PUSH_REPO"
+echo "TAGS: $TAGS"
+echo "PUSH_LATEST_TAG FLAG: $PUSH_LATEST_TAG"
+echo "PYTHON_VERSION FLAG: $PYTHON_VERSION"
+
+sleep 5
+export DOCKER_BUILDKIT=1
+docker buildx build $DOCKER_ARGS --build-arg PYTHON_VERSION=${PYTHON_VERSION} --platform $PLATFORM $TAGS .
+
 touch /tmp/version
 cat /dev/null > /tmp/version
 echo $IMAGE_VERSION > /tmp/version
-
-touch /tmp/duckdbx_version
-cat /dev/null > /tmp/duckdbx_version
-echo $DOCKER_DUCKX_IMAGE_VERSION > /tmp/duckdbx_version
