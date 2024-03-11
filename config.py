9d3022fb64db5929582e3fd30a2b7cc1340fd847
vishnu rao <jaihind213@gmail.com>
2024-03-11 13:45:02 +0800

    simplifying docker build (#17)
    * simplifying docker build
    * demo on pythonanywhere.com
    * upgrade to duckdb 0.10.0 to fix cve
    * fix cve
    HIGH CVE-2024-24762 --   pkg:pypi/fastapi@0.101.0
    HIGH CVE-2024-26130 -- pkg:pypi/cryptography@42.0.1
    HIGH CVE-2024-24762 --  pkg:pypi/starlette@0.27.0
    
    * fix cve
    HIGH CVE-2024-24762 -- pkg:pypi/starlette@0.35.1
    
    * fix cve
    CRITICAL CVE-2024-22682 --pkg:pypi/duckdb@0.9.0
    
    * upgrade duckdb
    CVE-2024-22682--pkg:pypi/duckdb@0.9.0
    
    * o-updated readme
    o-configuring secrets no longer required due to presence of secretmanager in duckdb
    
    * build arm64
    
    ---------
    
    Co-authored-by: vishnu chanderraju <vishnu.ch@cuezen.com>

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import configparser
-import logging
-import os
 
 app_config = configparser.SafeConfigParser()
 
@@ -12,35 +10,6 @@ def setup_app_config(default_ini):
 
 def get_config():
     return app_config
-
-
-# https://spacelift.io/blog/docker-secrets
-def configure_sekrets(connection):
-    """
-    sekret aka secret :)
-    :return: None
-    """
-    sekret_file = get_config().get(
-        "duck", "sekret_file", fallback="/run/secrets/duck_sekrets"
-    )
-
-    if os.path.isfile(sekret_file):
-        # if file not there error todo
-        logging.info("configuring secrets")
-        with open(sekret_file, "r") as file:
-            for line in file:
-                if not line.lstrip().startswith("#"):
-                    k, v = line.strip().split("=", 1)
-                    # duckdb specific
-                    # accesskeys/ secretkeys must have prefix duck_
-                    if k.startswith("duck_"):
-                        parameter = k[len("duck_") :]  # noqa: E203
-                        query = "set global {}='{}';".format(parameter, v)
-                        connection.execute(query)
-                    else:
-                        os.environ[k] = v
-    else:
-        logging.warning("no secrets file found!")
 
 
 # https://stackoverflow.com/questions/55179786/is-it-possible-to-provide-secret-to-docker-run
