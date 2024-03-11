61af0edf100e96293a51425e8c4c1a5ede5903d3
vishnu chanderraju <vishnu.ch@cuezen.com>
2024-03-11 12:15:09 +0800

    linting

--- api.py
+++ api.py
@@ -120,7 +120,7 @@ def run_sql(
 )
 def disclaimer():
     """
-    This api returns disclaimer for https://www.pythonanywhere.com/ demo website, kindly request to read it by trying out the api.
+    This api returns disclaimer for https://www.pythonanywhere.com/ demo website, kindly request to read it by trying out the api. # noqa: E501,B950
     """
     return "Disclaimer: if you are accessing radio-duck hosted on https://radioduck.pythonanywhere.com/ ,please not it is intended for demonstration purposes only. Do not store any sensitive information, passwords, or secrets on this server. The owner of this server is not liable for any data, including secrets, stored on the server that may be leaked. Use this service responsibly & at your own risk."  # noqa: E501,B950
 
