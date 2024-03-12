bc833bb66faa03731cb2cfc445107f3b153c66b5
vishnu chanderraju <vishnu.ch@cuezen.com>
2024-03-12 11:04:00 +0800

    changes for python anywhere

--- python-anywhere.py
+++ python-anywhere.py
@@ -1,10 +1,10 @@
 import server
 
 # entrypoint for pythonanywhere.com demo app
-server.load_config(["default.ini"])
+server.load_config(["/home/radioduck/radio-duck/pyanywhere.ini"])
 server.congfigure_logging()
 server.setup_duck()
 server.configure_router()
 
-# uvicorn python-anywhere:demo --app-dir '.'
-demo = server.app
+# uvicorn python-anywhere:app --app-dir '.'
+app = server.app
